7817ce18f0e395594333130041ef44e87c0cb65b
0-Wassila-0 <wassila.wsdi@gmail.com>
2024-01-12 10:04:50 +0100

    readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,40 @@
+// Références des éléments HTML
 var back = document.getElementById("back");
+var close = document.getElementById("close");
+//bouton pour ouvrir la modale app4Btn //
 var download = document.getElementById("download");
 var close = document.getElementById("close");
-var app1Btn = document.getElementById("app1Btn");
-var app1BtnDesk = document.getElementById("app1BtnDesk");
-var download1 = document.getElementsByClassName("download1");
-var download2 = document.getElementsByClassName("download2");
-console.dir(download1)
+var app1BtnDesk = document.getElementById("app1")
+var app1Btn = document.getElementById("app1Btn")
+var app4Btn = document.getElementsByClassName("app4Btn")
+console.dir(app4Btn);
 
+console.dir(back);
 
-
-console.dir(back)
-//back.style.display = "block";
+//back.style.display = "block"; //
 // Fonction de fermeture de la modale
 function closeModal() {
-    // Cache le modal à l'aide de CSS
-    // Réinitialise les variables
-    back.style.display = "none";
+  // Cache le modal à l'aide de CSS  
+  // Réinitialise les variables
+  back.style.display = "none";
 }
+
 // Fonction de déploiement de la modale
 function openModal() {
-    // Affiche le modal à l'aide de CSS 
-    // Réinitialise les variables
-    back.style.display = "block";
+  // Affiche le modal à l'aide de CSS
+  // Réinitialise les variables
+  back.style.display = "block";
+
 }
-download.addEventListener("click", openModal);
-app1Btn.addEventListener("click", openModal);
-app1BtnDesk.addEventListener("click", openModal);
-download1.addEventListener("click", openModal)
-download2.addEventListener("click", openModal)
-close.addEventListener("click", closeModal);
+download.addEventListener("click",openModal);
+close.addEventListener("click",closeModal);
+
+//action pour download 2 //
+download.addEventListener("click",openModal);
+app1BtnDesk.addEventListener("click",openModal);
+app1Btn.addEventListener("click",openModal);
+app4Btn[0].addEventListener("click",openModal);
+app4Btn[1].addEventListener("click",openModal);
+close.addEventListener("click",closeModal);
 
 
---
+++ readme.md
@@ -0,0 +1,47 @@
+
+# Project Title
+
+A brief description of what this project does and who it's for
+
+- first item
+- second item
+```echo "hello word```
+
+
+## Deployment
+
+To deploy this project run
+
+```bash
+node index.js```
+
+
+## Documentation
+
+[mon app2](https://github.com/0-Wassila-0/App2-maquette-2)
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+
+## Features
+
+- Light/dark mode toggle
+- Live previews
+- Fullscreen mode
+- Cross platform
+
+
+## Roadmap
+
+- Additional browser support
+
+- Add more integrations
+
